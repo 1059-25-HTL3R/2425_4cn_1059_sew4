23d412a73b154ca5427d7ac59977213380c12b59
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-09 13:32:17 +0100

    UE06 find_files_by_ext.py dokumentation geschrieben

--- UE06/find_files_by_ext.py
+++ UE06/find_files_by_ext.py
@@ -3,7 +3,7 @@ __author__ = 'benjamin Zwettler'
 import sys
 from pathlib import Path
 
-
+"""ein script das alle files rekursiv vom angegebenen startpunkt mit einer bestimmten ändung findet und deren aabsoluten pfad auf der konsole ausgibt"""
 sufix = input("Suffix nach dem zu suchen ist: ")
 p = Path(input("Bitte Pfad eingeben: "))
 if not p.is_dir():
