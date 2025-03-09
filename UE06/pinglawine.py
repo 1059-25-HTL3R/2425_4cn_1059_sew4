924f601a86b94b679125627423cf42bcad31dcf5
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-09 14:30:29 +0100

    UE06 pinglawine dokumentation

--- UE06/pinglawine.py
+++ UE06/pinglawine.py
@@ -4,6 +4,8 @@ __author__ = 'Benjamin Zwettler'
 import ipaddress
 import subprocess
 
+"""ein adminscript das eine ip + subnetsuffix einliest ud alle hosts in diesem netz pingt"""
+
 network = ipaddress.ip_network(input("bitte ip addresse mit netzsuffix eingeben"), strict=False)
 for ip in network.hosts():
     subprocess.run(["ping",str(ip)], creationflags=subprocess.CREATE_NEW_CONSOLE)
