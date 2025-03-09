4df36240c74392964ed826020e8f569ea27c95d6
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-09 14:13:54 +0100

    UE06 copy_dcim.py gefixed errors

--- UE06/copy_dcim.py
+++ UE06/copy_dcim.py
@@ -6,6 +6,8 @@ import shutil
 import sys
 from pathlib import Path
 
+"""ein adminscript das """
+
 p_src = Path(input("Bitte src Pfad eingeben: "))
 p_des = Path(input("Bitte des Pfad eingeben: "))
 if not p_des.is_dir() or not p_src.is_dir():
@@ -13,6 +15,9 @@ if not p_des.is_dir() or not p_src.is_dir():
     sys.exit()
 
 for file in p_src.glob(f'*{"jpg"}'):
+    if file.name.split('_')[0].__len__() != 8 or not file.name.split('_')[0].isdigit():
+        print("is not a real file" + str(file))
+        continue
     print(file)
     year = file.name.split('_')[0][:4]
     month = file.name.split('_')[0][4:6]
