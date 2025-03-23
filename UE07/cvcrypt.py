40cb296c406cafde490c814d914cc0a369f68e15
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-23 23:16:07 +0100

    UE07 cvcrypt.py funktions strucktur erstellt

--- UE07/cvcrypt.py
+++ UE07/cvcrypt.py
@@ -1,22 +1,41 @@
 __author__ = "Benjamin Zwettler"
 
 import argparse
-import pathlib
-import UE02.BetterKasiski as Kasiski
+import os
+
+def process_file(input_file: str, output_file: str, cipher: str, key, decrypt: bool, verbose: bool):
+    if not os.path.exists(input_file):
+        raise FileNotFoundError
 
 
-parser = argparse.ArgumentParser(description="cvcrypt.py parser to encrypt files")
+def main():
+    parser = argparse.ArgumentParser(description="cvcrypt.py parser to encrypt files")
+
+    parser.add_argument("chifre", help="which encrypting algoryth should be used",
+                        choices=["ceaser", "c", "vigenere", "v"])
+    parser.add_argument("infile", help="Path to the file you want to encrypt.")
+    parser.add_argument("outfile", help="Path where the encrypted file should be saved.")
+    parser.add_argument("-k", "--key", help="key for encryption", type=str, default="a", required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-e", "--encrypt", action="store_true", help="verschlüsseln")
+    group.add_argument("-d", "--decrypt", action="store_true", help="entschlüsseln")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Mehr Informationen anzeigen")  # Schalter
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(f"Verbose-Modus: {args.verbose}")
+        print(f"chifre: {args.chifre}")
+        print(f"srcPath: {args.infile}")
+        print(f"destPath: {args.outfile}")
+        print(f"key: {args.key}")
+        print(f"decrypt: {args.decrypt}")
+        print(f"encrypt: {args.encrypt}")
+    process_file(args.infile, args.outfile, args.cipher, args.key, args.decrypt, args.verbose)
 
 
-parser.add_argument("chifre", help="which encrypting algoryth should be used", type=str)
-parser.add_argument("src", "srcPath", help="Path to the file you want to encrypt.")
-parser.add_argument("dest","destPath", help="Path where the encrypted file should be saved.")
-parser.add_argument("-k", "--key", help="key for encryption", type=str, default="a")
-parser.add_argument("-a", "--alter", type=int, help="Dein Alter")  # Optionales Argument
-parser.add_argument("-v", "--verbose", action="store_true", help="Mehr Informationen anzeigen")  # Schalter
-
-args = parser.parse_args()
-
+if __name__ == "__main__":
+    main()
 
 
 
