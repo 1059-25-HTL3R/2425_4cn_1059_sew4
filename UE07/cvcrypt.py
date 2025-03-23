59f52fc562b1d1c953eabb049dfd4c6a65593e56
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-23 22:50:41 +0100

    UE07 cvcrypt.py parser erstellt

--- UE07/cvcrypt.py
+++ UE07/cvcrypt.py
@@ -1,4 +1,25 @@
 __author__ = "Benjamin Zwettler"
 
 import argparse
+import pathlib
 import UE02.BetterKasiski as Kasiski
+
+
+parser = argparse.ArgumentParser(description="cvcrypt.py parser to encrypt files")
+
+
+parser.add_argument("chifre", help="which encrypting algoryth should be used", type=str)
+parser.add_argument("src", "srcPath", help="Path to the file you want to encrypt.")
+parser.add_argument("dest","destPath", help="Path where the encrypted file should be saved.")
+parser.add_argument("-k", "--key", help="key for encryption", type=str, default="a")
+parser.add_argument("-a", "--alter", type=int, help="Dein Alter")  # Optionales Argument
+parser.add_argument("-v", "--verbose", action="store_true", help="Mehr Informationen anzeigen")  # Schalter
+
+args = parser.parse_args()
+
+
+
+
+
+
+
