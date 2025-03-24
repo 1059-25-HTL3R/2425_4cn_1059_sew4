63dbc333a1bc6723ada8076d3a244d8421106375
1059-25-HTL3R <1059@htl.rennweg.at>
2025-03-24 09:17:32 +0100

    UE07 cvcrack.py push

--- UE07/cvccrack.py
+++ UE07/cvccrack.py
@@ -2,3 +2,58 @@ __author__ = "Benjamin Zwettler"
 
 import argparse
 import UE02.BetterKasiski as Kasiski
+
+
+
+__author__ = "Benjamin Zwettler"
+
+import argparse
+import os
+from BetterKasiski import Ceaser, Kasinski
+
+def process_file(input_file: str, output_file: str, chifre: str, verbose: bool, n:int):
+    if not os.path.exists(input_file):
+        raise FileNotFoundError
+
+    infile = open(input_file, 'r')
+    outfile = open(output_file, 'w')
+
+    if verbose:
+        print()
+
+
+
+    if chifre in ["ceaser", "c"]:
+        ceaser = Ceaser()
+        outfile.write(str(ceaser.crack(infile.read())))
+    elif chifre in ["vigenere", "v"]:
+        kasinski = Kasinski(infile.read())
+        outfile.write(kasinski.crack_key())
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="cvcrypt.py parser to encrypt files")
+
+    parser.add_argument("chifre", help="which encrypting algorythm should be used",
+                        choices=["ceaser", "c", "vigenere", "v"])
+    parser.add_argument("infile", help="Path to the file you want to encrypt.")
+    parser.add_argument("outfile", help="Path where the encrypted file should be saved.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    parser.add_argument("-v", "--verbose", action="store_true", help="Mehr Informationen anzeigen")  # Schalter
+    parser.add_argument("-n", "--number", help="how many possible keys are to be calculated",type=int)  # Schalter
+
+    args = parser.parse_args()
+
+    process_file(args.infile, args.outfile, args.chifre, args.verbose, args.n)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
