6e1e469866f84731d61fed00aa22093f4a7e917b
1059-25-HTL3R <1059@htl.rennweg.at>
2025-04-26 17:52:09 +0200

    UE09 Fractions variablen private gemacht getter geschrieben

--- UE09_Fraction/Fraction.py
+++ UE09_Fraction/Fraction.py
@@ -8,76 +8,84 @@ class Fraction:
     def __init__(self, numerator=0, denominator=1):
         if denominator == 0:
             raise ArithmeticError("denominator cannot be 0")
-        self.numerator = numerator
-        self.denominator = denominator
+        self._numerator = numerator
+        self._denominator = denominator
 
     def __str__(self):
         self.kuerzen()
-        if self.numerator >= self.denominator:
-            ganze = int(self.numerator / self.denominator)
-            rest = self.numerator % self.denominator
+        if self._numerator >= self._denominator:
+            ganze = int(self._numerator / self._denominator)
+            rest = self._numerator % self._denominator
             if rest != 0:
-                return str(ganze) + " " + str(rest) +"/"+ str(self.denominator)
+                return str(ganze) + " " + str(rest) +"/"+ str(self._denominator)
             else:
                 return str(ganze)
         else:
-            return str(self.numerator) + "/" + str(self.denominator)
+            return str(self._numerator) + "/" + str(self._denominator)
 
     def __repr__(self) -> str:
         """
         :return: a string representation of the Fraction
         """
         self.kuerzen()
-        return f"Fraction({self.numerator}/{self.denominator})"
+        return f"Fraction({self._numerator}/{self._denominator})"
 
     def kuerzen(self):
-        greatestteiler = gcd(self.numerator, self.denominator)
-        self.numerator = self.numerator // greatestteiler
-        self.denominator = self.denominator // greatestteiler
-        return Fraction(self.numerator, self.denominator)
+        greatestteiler = gcd(self._numerator, self._denominator)
+        self._numerator = self._numerator // greatestteiler
+        self._denominator = self._denominator // greatestteiler
+        return Fraction(self._numerator, self._denominator)
 
     def __add__(self, other):
         if isinstance(other, Fraction):
-            frac1 = Fraction(self.numerator, self.denominator * other.denominator)
-            frac1.numerator = (frac1.numerator * other.denominator) + (other.numerator * self.denominator)
+            frac1 = Fraction(self._numerator, self._denominator * other._denominator)
+            frac1._numerator = (frac1._numerator * other._denominator) + (other._numerator * self._denominator)
             return frac1.kuerzen()
 
     def __sub__(self, other):
         if isinstance(other, Fraction):
-            frac1 = Fraction(self.numerator, self.denominator * other.denominator)
-            frac1.numerator = (frac1.numerator * other.denominator) - (other.numerator * self.denominator)
+            frac1 = Fraction(self._numerator, self._denominator * other._denominator)
+            frac1._numerator = (frac1._numerator * other._denominator) - (other._numerator * self._denominator)
             return frac1.kuerzen()
 
     def __mul__(self, other):
         if isinstance(other, Fraction):
-            frac1 = Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
+            frac1 = Fraction(self._numerator * other._numerator, self._denominator * other._denominator)
             return frac1.kuerzen()
 
     def __truediv__(self, other):
         """wenn / geschrieben wird"""
         if isinstance(other, Fraction):
-            frac1 = Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
+            frac1 = Fraction(self._numerator * other._denominator, self._denominator * other._numerator)
             return frac1.kuerzen()
 
     def __floordiv__(self, other):
         """wenn // geschrieben wird"""
         if isinstance(other, Fraction):
-            frac1 = Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
+            frac1 = Fraction(self._numerator * other._denominator, self._denominator * other._numerator)
             return frac1.kuerzen()
 
     def __eq__(self, other):
         if isinstance(other, Fraction):
-            return self.numerator == other.numerator and self.denominator == other.denominator
+            return self._numerator == other._numerator and self._denominator == other._denominator
         if isinstance(other, int):
             return NotImplemented
         return NotImplemented
 
     def __lt__(self, other):
         if isinstance(other, Fraction):
-            return self.numerator / self.denominator < other.numerator / other.denominator
+            return self._numerator / self._denominator < other._numerator / other._denominator
         if isinstance(other, int):
             return NotImplemented
         return NotImplemented
+
+    def _numerator(self):
+        return self._numerator
+
+    def _denominator(self):
+        return self._denominator
+
+
 
 
 
@@ -98,4 +106,5 @@ if __name__ == "__main__":
 
     print((fraction1 // fraction2))
 
-    print((fraction1 < 1))
+
+    print(fraction1._numerator)
